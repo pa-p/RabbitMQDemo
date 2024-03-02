809fdd54c2cbcc74e3899e4ac0bbb651365cd022
Pawel P <pawlukojc.p@gmail.com>
2024-03-02 21:49:43 +0100

    Message sending.

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -3,3 +3,22 @@ using System.Text;
 
 ConnectionFactory factory = new();
 factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello!");
+channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+channel.Close();
+cnn.Close();
